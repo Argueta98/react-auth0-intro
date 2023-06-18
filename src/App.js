8490ad34bf10e8ee04e472a578d5acc83624097b
Argueta Contreras Jesiel Abrahm <cheargueta982015jhos@gmail.com>
2023-06-17 23:26:18 -0600

    Agregando Aminlte y paginas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import './App.css';
 import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
+import Conten from './components/Content';
+import Aside from './components/Aside';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import Profile from './components/Profile';
+import Home from './components/Home';
 import { useAuth0 } from '@auth0/auth0-react';
 
 function App() {
@@ -10,11 +15,16 @@ function App() {
   if(isLoading) return <h1>Loading ...</h1>
   return (
     <div className="App">
-      <h1>APLICACION DE REACT</h1>
+
       {
-        isAuthenticated ? <LogoutButton /> : <LoginButton />
+        isAuthenticated ? 
+        <Header />
+        : 
+        <Home />
       }
       <Profile />
+      <Aside /> 
+      <Footer />
 
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,97 @@
+import React from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+function Header() {
+  const { user, isAuthenticated, logout } = useAuth0();
+  return (
+    isAuthenticated && (
+      <nav className="main-header navbar navbar-expand navbar-white navbar-light">
+        {/* Left navbar links */}
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="pushmenu"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-bars" />
+            </a>
+          </li>
+          <li className="nav-item d-none d-sm-inline-block">
+            <a href="index3.html" className="nav-link">
+              Home
+            </a>
+          </li>
+          <li className="nav-item d-none d-sm-inline-block">
+            <a href="#" className="nav-link">
+              Contact
+            </a>
+          </li>
+        </ul>
+        {/* Right navbar links */}
+        <ul className="navbar-nav ml-auto">
+          {/* Navbar Search */}
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="navbar-search"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-search" />
+            </a>
+            <div className="navbar-search-block">
+              <form className="form-inline">
+                <div className="input-group input-group-sm">
+                  <input
+                    className="form-control form-control-navbar"
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                  />
+                  <div className="input-group-append">
+                    <button className="btn btn-navbar" type="submit">
+                      <i className="fas fa-search" />
+                    </button>
+                    <button
+                      className="btn btn-navbar"
+                      type="button"
+                      data-widget="navbar-search"
+                    >
+                      <i className="fas fa-times" />
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </li>
+          {/* Messages Dropdown Menu */}
+
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              data-widget="fullscreen"
+              href="#"
+              role="button"
+            >
+              <i className="fas fa-expand-arrows-alt" />
+            </a>
+          </li>
+          <li className="nav-item">
+            <a
+              className="nav-link"
+              href="#"
+              role="button"
+              onClick={() => logout()}
+            >
+              <i className="fas fa-sign-out-alt" /> Salir
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  );
+}
+
+export default Header;
